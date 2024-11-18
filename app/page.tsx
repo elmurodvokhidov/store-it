7742cf9e485f082bbbba16c0cbddf71300fdd47c
unknown <elmurodvkh@gmail.com>
2024-11-18 16:37:54 +0500

    design system, theming & tailwindcss config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl">Store It - The only storage solution you need.</h1>
+    <div className="flex-center h-screen">
+      <h1 className="h1">Store It - The only storage solution you need.</h1>
     </div>
   );
 }
