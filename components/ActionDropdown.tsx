e1bf758a8384c03321202c8f108fef8373bd90ce
unknown <elmurodvkh@gmail.com>
2024-12-01 17:25:03 +0500

    rename file

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -23,6 +23,8 @@ import { Models } from "node-appwrite";
 import { useState } from "react"
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { renameFile } from "@/lib/actions/file.actions";
+import { usePathname } from "next/navigation";
 
 export default function ActionDropdown({ file }: { file: Models.Document }) {
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -30,6 +32,7 @@ export default function ActionDropdown({ file }: { file: Models.Document }) {
     const [action, setAction] = useState<ActionType | null>(null);
     const [name, setName] = useState(file.name);
     const [isLoading, setIsLoading] = useState(false);
+    const path = usePathname();
 
     const renderDialogContent = () => {
         if (!action) return null;
@@ -43,7 +46,22 @@ export default function ActionDropdown({ file }: { file: Models.Document }) {
             // setEmails([]);
         };
 
-        const handleAction = async () => { }
+        const handleAction = async () => {
+            if (!action) return;
+            setIsLoading(true);
+            let success = false;
+
+            const actions = {
+                rename: () => renameFile({ fileId: file.$id, name, extension: file.extension, path }),
+                share: () => console.log("share"),
+                delete: () => console.log("delete"),
+            };
+
+            success = await actions[action.value as keyof typeof actions]();
+
+            if (success) closeAllModals();
+            setIsLoading(false);
+        }
 
         return (
             <DialogContent className="shad-dialog button">
